d68caa60b615e8c9e9f2ac88d70088b167fae4c9
obazeec <obazee.esosa@yahoo.com>
2025-01-09 19:30:16 +0000

    Core completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,39 +5,37 @@ class Program
     static void Main(string[] args)
     {
         string ans = "";
-        int magicNumber;
+        //int magicNumber;
         int guessNumber;
+        int number ;
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-      
-   do
-   {
-
-   
-        Console.Write("What is the magic number: ");
-        ans = Console.ReadLine();
-        magicNumber = int.Parse(ans);
-        Console.Write("What is your guess: ");
-        ans =   Console.ReadLine();
-        guessNumber = int.Parse(ans);
-        if (guessNumber > magicNumber)
-        {
-            Console.WriteLine("Guess lower");
-
-        }
-        else if ( guessNumber < magicNumber)
-        {
-            Console.WriteLine("Guess higher");
-        }
-
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
-
-       } while (guessNumber != magicNumber); 
+        Random r = new Random();
+        number = r.Next(1, 100);
+    do
+    {
         
+    
+            //Console.Write("What is the magic number: ");
+            //ans = Console.ReadLine();
+            //magicNumber = int.Parse(ans);
+            Console.Write("What is your guess: ");
+            ans =   Console.ReadLine();
+            guessNumber = int.Parse(ans);
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Guess lower");
 
+            }
+            else if ( guessNumber < number)
+            {
+                Console.WriteLine("Guess higher");
+            }
 
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
 
+        } while (guessNumber != number); 
     }
 }
