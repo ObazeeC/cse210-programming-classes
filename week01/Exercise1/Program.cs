7bfdc48ef8355880fe25e4c002eece06d65cd7c7
obazeec <obazee.esosa@yahoo.com>
2025-01-09 08:22:46 +0000

    Input and Output Programming Exercise completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        string firstName;
+        string lastName;
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+       
+        Console.Write("What is your first name ? ");
+        firstName = Console.ReadLine();
+
+
+        Console.Write("What is your last name ? ");
+        lastName = Console.ReadLine();
+        
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
