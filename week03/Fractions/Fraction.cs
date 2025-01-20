8ffd09a6b32de3521f6ce796cb65bf4cd737a5ba
obazeec <obazee.esosa@yahoo.com>
2025-01-20 18:43:34 +0000

    Learning activity Encapsulation completed

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,58 @@
+using System;
+public class Fraction
+{
+private int _top = 0;
+private int _bottom = 0;
+
+//CREATE THE CONSTRUCTORS
+public Fraction()
+{
+    _top = 1;
+    _bottom = 1;
+}
+public Fraction(int top)
+{
+    _top = top;
+    _bottom = 1;
+}
+public Fraction(int top, int bottom)
+{
+    _top = top;
+    _bottom = bottom;
+}
+
+//CREATE THE GETTERS AND SETTERS
+public int GetNumerator()
+{
+    return _top;
+}
+
+public int SetNumerator(int top)
+{
+    _top = top;
+    return _top;
+    
+}
+
+public int SetDenominator(int bottom)
+{
+    _bottom = bottom;
+    return _bottom;
+    
+}
+public int GetDenominator()
+{
+    return _bottom;
+}
+public string GetFractionString()
+{
+    string fraction = ($"{_top}/{_bottom}") ;
+    return fraction;
+}
+public double GetDecimalValue()
+{
+    double top = _top;
+    double bottom = _bottom;
+    return top / bottom;
+}
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Console.WriteLine("Encapsulation! This is the Fractions Project.");
+        Console.WriteLine("---------------------------------------------");
+
+        Fraction f1 = new ();
+        f1.GetDenominator();
+        f1.SetDenominator(2);
+   
+        
+        Fraction f2 = new(6 ,8);
+         Console.WriteLine(f2.GetNumerator());
+         f2.SetNumerator(3);
+         Console.WriteLine("---------------------------------------------");
+         f2.GetFractionString();
+     
+        Console.WriteLine("f3"); 
+        Fraction f3 = new(3,4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine("f4"); 
+        Fraction f4 = new(); 
+        f4.SetNumerator(1);
+        f4.SetDenominator(3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+
     }
 }
