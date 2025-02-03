f524a487a44012be4c9cfa56ca1ddf72ff014d7a
obazeec <obazee.esosa@yahoo.com>
2025-02-03 21:20:19 +0000

    Learning

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine("Inheritance ! This is the Homework Project.");
+        Console.WriteLine("");
+
+        Assignment a1 = new("Esosa Obazee", "Physics");
+       // Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new("Esosa Obazee", "Biology", "12.1", "Anatomy");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
     }
 }
